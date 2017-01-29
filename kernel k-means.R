3718639a8edd8659627dc0f858013f4ffb9f05df
henry-p <henry.perschk@gmail.com>
2017-01-29 23:36:15 +0100

    some "working" implementation of kkmeans (graph looks wrong...)

--- kernel k-means.R
+++ kernel k-means.R
@@ -1,4 +1,17 @@
 library(kernlab)
 
-kkmeans(hotels.features * 1, centers = 10, kernel = "polydot")
-kkmeans(hotels.features * 1, centers = 10, kernel = "polydot")
+m = as.matrix(hotels.features) * 1
+
+totalwss = 0
+maxNrOfClusters = 15
+for (i in seq_len(maxNrOfClusters)) {
+  km = NA
+  while(is.na(km)) {
+    tryCatch({
+      km = kkmeans(m, centers = i, kernel = "polydot")
+    }, error=function(e){})
+  }
+  totalwss[i] = sum(km@withinss)
+}
+
+plot(seq_len(maxNrOfClusters), totalwss, type="b", xlab="Number of Clusters", ylab="Within groups sum of squares")
